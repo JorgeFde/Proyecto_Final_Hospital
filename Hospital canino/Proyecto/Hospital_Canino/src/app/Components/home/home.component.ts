c3eedcdbd9878ee120adaf27f883530d9c1da6f7
Jorge Armando Fernandez G <jorgearmando177@gmail.com>
2025-05-04 15:04:51 -0600

    fix-commit creacion de cambios en el home
    se cambia la distribucion del home dejando el diseño de la mitad de la pantalla

--- Hospital canino/Proyecto/Hospital_Canino/src/app/Components/home/home.component.ts
+++ Hospital canino/Proyecto/Hospital_Canino/src/app/Components/home/home.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 @Component({
   selector: 'app-home',
-  imports: [MatToolbar],
+  imports: [MatToolbar, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  animations: [
+    trigger('fade', [
+      state('visible', style({ opacity: 1 })),
+      state('hidden', style({ opacity: 0 })),
+      transition('visible <=> hidden', animate('1s ease-in-out')),
+    ])
+  ]
+
 })
 export class HomeComponent {
-
+  // arreglo de imagenes para animaciones con textos
+  images = [
+    { src: 'assets/images/banner1.jpg', text: 'QUIRÓFANO' },
+    { src: 'assets/images/banner2.jpg', text: 'CARDIOLOGIA' },
+    { src: 'assets/images/banner3.jpg', text: 'LABORATORIO' }
+  ];
+  currentIndex = 0;
+  animationState: 'visible' | 'hidden' = 'visible';
+  ngOnInit() {
+    this.startLoop();
+  }
+  // funcion para loop de animacion
+  startLoop() {
+    setInterval(() => {
+      this.animationState = 'hidden';
+      // Espera a que termine la animación de salida para cambiar de imagen
+      setTimeout(() => {
+        this.currentIndex = (this.currentIndex + 1) % this.images.length;
+        this.animationState = 'visible';
+      }, 700); // mismo tiempo que la animación (1s)
+    }, 6000); // duración total por imagen (2s visible + 1s fade)
+  }
 }
--- Hospital canino/Proyecto/Hospital_Canino/src/main.ts
+++ Hospital canino/Proyecto/Hospital_Canino/src/main.ts
@@ -1,6 +1,9 @@
+// Se modifica el main ts para agregar la anumacuones del provideAnimations
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideAnimations } from '@angular/platform-browser/animations'; // ✅ IMPORTANTE
 import { AppComponent } from './app/app.component';
-
+const appConfig = {
+  providers: [provideAnimations()] // ✅ AÑADIDO
+};
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
