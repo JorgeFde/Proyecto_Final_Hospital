e335030f59dbfa2aa16c0825a6f75e52c49627eb
Jorge Armando Fernandez G <jorgearmando177@gmail.com>
2025-05-04 12:22:58 -0600

    fix-Implementacion de proyecto

---
+++ Hospital canino/Proyecto/Hospital_Canino/src/app/Components/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+@Component({
+  selector: 'app-home',
+  imports: [MatToolbar],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
