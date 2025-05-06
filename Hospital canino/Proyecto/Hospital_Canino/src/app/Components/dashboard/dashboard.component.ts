2a7a3b1ffbfaf6e6b0c0cc9a18cbd3a70134e410
Jorge Armando Fernandez G <jorgearmando177@gmail.com>
2025-05-06 13:26:44 -0600

    fix-correccion de dashboaard

--- Hospital canino/Proyecto/Hospital_Canino/src/app/Components/dashboard/dashboard.component.ts
+++ Hospital canino/Proyecto/Hospital_Canino/src/app/Components/dashboard/dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatDivider } from '@angular/material/divider';
-import { MatIcon } from '@angular/material/icon';
-import { NgIf, NgStyle } from '@angular/common';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-dashboard',
-  imports: [MatToolbar, MatDivider, MatIcon, NgIf, NgStyle],
+  imports: [MatToolbar, MatDivider, NgIf],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
