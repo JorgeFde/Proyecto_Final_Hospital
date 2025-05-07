e1691ad50dce70cff8dab590ffb53650c255eaf1
Jorge Armando Fernandez G <jorgearmando177@gmail.com>
2025-05-07 00:43:58 -0600

    fix-commit implementacion de chats y dashboard
    Se implmento el dash board pero faltan los detalles de cada opcion y se implemento el diseño de chat del admin y de cliente

--- Hospital canino/Proyecto/Hospital_Canino/src/app/Components/dashboard/dashboard.component.ts
+++ Hospital canino/Proyecto/Hospital_Canino/src/app/Components/dashboard/dashboard.component.ts
@@ -1,17 +1,57 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
-import { MatDivider } from '@angular/material/divider';
 import { NgIf } from '@angular/common';
+import { NgClass } from '@angular/common';
+import { NotificationsComponent } from "../notifications/notifications.component";
 @Component({
   selector: 'app-dashboard',
-  imports: [MatToolbar, MatDivider, NgIf],
+  imports: [MatToolbar, NgIf, NgClass, NotificationsComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
   isOpen: 'activos' | 'pendientes' | 'completos' | null = null;
-
+  panelVisible: boolean = false;
+  isNotification: boolean = false;
+  isActiveAnimation: boolean = false
+  intervalId: any;
+  bellAnimating: boolean = false;
+  ngOnInit() {
+    this.setupNotificationAnimationLoop();
+  }
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
   toggle(panel: 'activos' | 'pendientes' | 'completos') {
     this.isOpen = this.isOpen === panel ? null : panel;
+  }
+  // panel de notificaciones
+  togglePanel() {
+    this.panelVisible = !this.panelVisible;
+  }
+  // aniacion de camapana
+  setupNotificationAnimationLoop() {
+    if (this.isNotification) {
+      this.triggerShake();
+  
+      this.intervalId = setInterval(() => {
+        if (this.isNotification) {
+          this.triggerShake();
+        }
+      }, 1 * 60 * 1000); // cada 1 minutos
+    }
+  }
+  triggerShake() {
+    // activa animación
+    this.bellAnimating = true;
+    // detiene animación después de 2 segundos
+    setTimeout(() => {
+      this.bellAnimating = false;
+    }, 2000);
+  }
+  // termina animacion de campana
+  // cerrar sesion
+  toggleLogOut() {
+
   }
 }
